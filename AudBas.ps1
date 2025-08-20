5ed1f1807a5e4b062456bb5d825769c346a9364c
fer-gza <fernando.garzach@uanl.edu.mx>
2025-08-20 10:26:48 -0600

    Create AudBas.ps1

---
+++ AudBas.ps1
@@ -0,0 +1,16 @@
+﻿#Analizador de procesos - con otro foreach
+$procesos = Get-Process
+$alertas = @()
+foreach ($p in $procesos){
+    if ($p.WorkingSet -gt 100MB){
+        $alertas +="$($p.Name) usa $([math]::Round($p.WorkingSet / 1MB, 2)) MB"
+    }
+}
+if ($alertas.Count -gt 0){
+    Write-Output "Procesos con alto consumo:"
+    foreach($a in $alertas){
+        Write-Output $a
+    }
+} else {
+    Write-Output "No hay procesos críticos"
+}
